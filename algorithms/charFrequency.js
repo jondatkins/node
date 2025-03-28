dbb6b4b7967575149a5ef1ae79e6d94d9eef4eab
jondatkins <jon.david.atkins@gmail.com>
2025-03-28 21:08:32 +0000

    Some error checking for charFrequency, more test cases.

--- algorithms/charFrequency.js
+++ algorithms/charFrequency.js
@@ -1,6 +1,10 @@
 // For a given input string, get the frequency of characters in the string.
 
 function charFrequency(str) {
+  if (!str) {
+    console.log(`Function requires a string argument, value passed was ${str}`);
+    return;
+  }
   let frequency = {};
 
   [...str].forEach((element) => {
@@ -16,5 +20,11 @@ function charFrequency(str) {
 
 result = charFrequency('hello'); // { h: 1, e: 1, l: 2, o: 1 }
 console.log(result);
-result2 = charFrequency('abbcccdddd'); // { h: 1, e: 1, l: 2, o: 1 }
+result2 = charFrequency('abbcccdddd'); // {a: 1, b: 2, c: 3, d: 4}
 console.log(result2);
+result3 = charFrequency(); // undefinced
+console.log(result3);
+result4 = charFrequency(undefined); // undefined
+console.log(result4);
+result5 = charFrequency(null); // null
+console.log(result5);
