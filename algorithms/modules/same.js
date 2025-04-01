6f065f099d17e75d7a482ac13e5cd5f3ef6f8121
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2025-04-01 21:30:12 +0100

    Second solution, this time with objects.

--- algorithms/modules/same.js
+++ algorithms/modules/same.js
@@ -1,10 +1,11 @@
-function same(array1, array2) {
+var _ = require('lodash');
+
+const same = (array1, array2) => {
   if (array1.length !== array2.length) return false;
   array1 = array1.sort();
   array2 = array2.sort();
   // let array3 = array1.map((item) => item * item));
   const array3 = array1.map((x) => x * x);
-  let arraysMatch = false;
   for (let i = 0; i < array2.length; i++) {
     if (array2[i] != array3[i]) {
       return false;
@@ -13,5 +14,20 @@ function same(array1, array2) {
   return true;
 }
 
-// export { same };
-exports.same = same;
+const same2 = (array1, array2) => {
+  if (array1.length !== array2.length) return false;
+  let obj1 = {};
+  let obj2 = {};
+  array1.forEach(element => {
+    obj1[element * element] = ++obj1[element * element] || 1;
+  });
+  array2.forEach(element => {
+    obj2[element] = ++obj2[element] || 1;
+  });
+  if (_.isEqual(obj1, obj2)) {
+    return true;
+  }
+  return false;
+}
+
+module.exports = { same, same2 };
--- algorithms/tests/same.test.js
+++ algorithms/tests/same.test.js
@@ -1,11 +1,21 @@
 const same = require('../modules/same');
 
+// test(``, () => {
+//   expect(same.same([1, 2, 3], [4, 1, 9])).toBe(true)
+// })
+// test(``, () => {
+//   expect(same.same([1, 2, 3], [4, 5, 9])).toBe(false)
+// })
+// test(``, () => {
+//   expect(same.same([1, 2, 1], [4, 4, 1])).toBe(false)
+// })
+//
 test(``, () => {
-  expect(same.same([1, 2, 3], [4, 1, 9])).toBe(true)
+  expect(same.same2([1, 2, 3], [4, 1, 9])).toBe(true)
 })
 test(``, () => {
-  expect(same.same([1, 2, 3], [4, 5, 9])).toBe(false)
+  expect(same.same2([1, 2, 3], [4, 5, 9])).toBe(false)
 })
 test(``, () => {
-  expect(same.same([1, 2, 1], [4, 4, 1])).toBe(false)
+  expect(same.same2([1, 2, 1], [4, 4, 1])).toBe(false)
 })
