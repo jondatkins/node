031352ad2dcd4ad04bafdda72fc5c7d61c3c76fa
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2025-04-09 15:54:54 +0100

    Two fixes for binary search.

--- algorithms/modules/binarySearch.js
+++ algorithms/modules/binarySearch.js
@@ -3,27 +3,54 @@ const binarySearch = (searchArray, numbToSearch) => {
   if (searchArray.length === 0) {
     return -1;
   }
-  let i = Math.trunc((searchArray.length / 2));
-  let range = Math.trunc((searchArray.length / 2) - 1);
-  while (i >= 0 && i < searchArray.length) {
-    if (searchArray[i] === numbToSearch) {
-      return i;
+  // let mid = Math.trunc((searchArray.length / 2));
+  let mid = 0;
+  let low = 0;
+  let high = Math.trunc(searchArray.length - 1);
+
+  while (low <= high) {
+    mid = Math.floor((high + low) / 2);
+    // look in left half, excluding middle
+    if (searchArray[mid] < numbToSearch) {
+      low = mid + 1;
     }
-    else if (searchArray[i] > numbToSearch) {
-      i = i - range;
+    else if (searchArray[mid] > numbToSearch) {
+      high = mid - 1;
     }
     else {
-      i = i + range;
+      return mid;
     }
-    range = Math.trunc(range / 2) - 1;
   }
   return -1;
 }
 
-
 // binarySearch([1, 2, 3, 4, 5, 6], 4) // 3
 // binarySearch([1, 2, 3, 4, 5, 6], 6) // 5
 // binarySearch([1, 2, 3, 4, 5, 6], 11) // -1
-binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)
+// binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)
 
-module.exports = { binarySearch };
+const arr = [1, 2, 3, 4, 5, 6]
+
+function findMe(target, start, end) {
+  if (start > end) {
+    return -1;
+  }
+  const middle = Math.floor((start + end) / 2);
+  if (arr[middle] === target) {
+    return middle;
+  }
+
+  if (arr[middle] > target) {
+    return findMe(target, start, middle - 1);
+  }
+
+  if (arr[middle] < target) {
+    return findMe(target, middle + 1, end)
+  }
+}
+// console.log(findMe(3, 0, arr.length - 1));
+console.log(findMe(5, 0, arr.length - 1));
+// console.log(findMe(1, 0, arr.length - 1));
+// console.log(findMe(7, 0, arr.length - 1));
+
+module.exports = { binarySearch, findMe };
