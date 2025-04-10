1e9b0b8c1512d7dc7cb68435268e05074e4e5162
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2025-04-10 16:23:56 +0100

    Sliding window fix for duplicate arguments

--- algorithms/modules/areThereDuplicates.js
+++ algorithms/modules/areThereDuplicates.js
@@ -1,4 +1,3 @@
-// Count the unique values in a sorted array, and return that value
 const areThereDuplicates = (...theArgs) => {
   let argsFrequency = {};
   for (const arg of theArgs) {
@@ -6,14 +5,39 @@ const areThereDuplicates = (...theArgs) => {
       return true;
     }
     else {
-      argsFrequency[arg] = arg;
+      argsFrequency[arg] = 1;
     }
   }
   return false;
 }
 
-areThereDuplicates(1, 2, 3) // false
-areThereDuplicates(1, 2, 2) // true 
-areThereDuplicates('a', 'b', 'c', 'a') // true 
+// areThereDuplicates(1, 2, 3) // false
+// areThereDuplicates(1, 2, 2) // true 
+// areThereDuplicates('a', 'b', 'c', 'a') // true 
 
-module.exports = { areThereDuplicates };
+const areThereDuplicates2 = (...theArgs) => {
+  let argsFrequency = {};
+  let i = 0;
+  let j = i + 1;
+  while (i < theArgs.length) {
+    if (j > theArgs.length - 1) {
+      // compare i and j
+      j = 0;
+      if (theArgs[i] === theArgs[j]) {
+        return true;
+      }
+    }
+    if (theArgs[i] === theArgs[j]) {
+      return true;
+    }
+    i++;
+    j++;
+  }
+  return false;
+}
+
+areThereDuplicates2(1, 2, 3) // false
+areThereDuplicates2(1, 2, 2) // true 
+areThereDuplicates2('a', 'b', 'c', 'a') // true 
+
+module.exports = { areThereDuplicates, areThereDuplicates2 };
--- algorithms/tests/areThereDuplicates.test.js
+++ algorithms/tests/areThereDuplicates.test.js
@@ -1,11 +1,20 @@
 const areThereDupes = require('../modules/areThereDuplicates');
 
-test(`There are duplicates for the args 1, 2, 3`, () => {
+test(`There are no duplicates for the args 1, 2, 3`, () => {
   expect(areThereDupes.areThereDuplicates(1, 2, 3)).toBe(false)
 })
 test(`There are duplicates for the args 1, 2, 2`, () => {
   expect(areThereDupes.areThereDuplicates(1, 2, 2)).toBe(true)
 })
-test(`There are no duplicates for the args 'a','b','c','a'`, () => {
+test(`There are duplicates for the args 'a','b','c','a'`, () => {
   expect(areThereDupes.areThereDuplicates('a', 'b', 'c', 'a')).toBe(true)
+})
+test(`There are no duplicates for the args 1, 2, 3`, () => {
+  expect(areThereDupes.areThereDuplicates2(1, 2, 3)).toBe(false)
+})
+test(`There are duplicates for the args 1, 2, 2`, () => {
+  expect(areThereDupes.areThereDuplicates2(1, 2, 2)).toBe(true)
+})
+test(`There are duplicates for the args 'a','b','c','a'`, () => {
+  expect(areThereDupes.areThereDuplicates2('a', 'b', 'c', 'a')).toBe(true)
 })
