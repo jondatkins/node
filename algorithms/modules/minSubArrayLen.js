4c48ac4227a3e87ce42db4f67eabad0806d75102
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2025-04-25 21:52:32 +0100

    All tests pass for minSubArrayLength problem.

--- algorithms/modules/minSubArrayLen.js
+++ algorithms/modules/minSubArrayLen.js
@@ -3,31 +3,43 @@
 // or equal to the integer passed to the function. If 
 // there isn't one, return 0 instead.
 const minSubArrayLen = (intArray, num) => {
-  let tempSum = 0;
+  let tempSum = intArray[0];
+  let subarrayLength = intArray.length;
   let minLength = 0;
-  for (let i = 0; i < intArray.length; i++) {
-    tempSum += intArray[i];
+  let start = 0;
+  let end = 0;
+  while (start < intArray.length && end < intArray.length) {
     if (tempSum >= num) {
-      minLength = i;
-      break;
+      let tempSubarrayLength = end - start + 1;
+      subarrayLength = Math.min(tempSubarrayLength, subarrayLength);
+      // If we're looking at a 1 length subarray, this is the smallest possible.
+      if (start === end) {
+        return subarrayLength;
+      }
+      tempSum = tempSum - intArray[start];
+      start++;
+    }
+    else {
+      end++;
+      tempSum += intArray[end];
+      if (end === intArray.length && start === 0) {
+        subarrayLength = 0;
+      }
     }
   }
-  for (let i = minLength; i < intArray.length; i++) {
-    // loop stuff here
-
-    tempSum = tempSum - intArray[i - minLength];
-    tempSum += intArray[i];
-    if (tempSum >= num && i + 1 < minLength) {
-      minLength = i + 1;
-    }
-  }
-  return minLength;
+  return subarrayLength;
 }
-console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)) // 2 -> because [4,3] is the smallest subarray; 
+// console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)) // 2 -> because [4,3] is the smallest subarray; 
 // console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)) // 2 -> because [5,4] is the smallest subarray; 
 // console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)) // 1 -> because [62] is greater than 52; 
 // console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39)) // 3; 
 // console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)) // 5; 
 // console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)) // 2; 
-// console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95)) // 0; 
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95)) // 0; 
+
+const printArray = (anArray, start, end) => {
+  for (let i = start; i < end; i++) {
+    console.log(anArray[i]);
+  }
+}
 module.exports = { minSubArrayLen };
