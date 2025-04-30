da73062d40635c929b490f89e9e8db6451f4a3e0
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2025-04-30 21:33:58 +0100

    Looking at course solution to 'findLongestSubstring' compared to my own

--- algorithms/modules/findLongestSubstring2.js
+++ algorithms/modules/findLongestSubstring2.js
@@ -14,15 +14,17 @@ const findLongestSubstring = (someString) => {
   let charIndices = {};
   while (end < someString.length) {
     loopCount++;
+    let char = someString[end];
     // Add character to set if it isn't there already, and increment
     // end pointer.
-    if (!charIndices.hasOwnProperty(someString[end])) {
-      charIndices[someString[end]] = end;
+    if (!charIndices.hasOwnProperty(char)) {
+      charIndices[char] = end;
       end++;
     }
     else {
       // Get the index of the duplicate character.
-      start = charIndices[someString[end]] + 1;
+      start = charIndices[char] + 1;
+      start = Math.max(start, charIndices[char] + 1);
       end = start;
       charIndices = {};
     }
@@ -32,11 +34,11 @@ const findLongestSubstring = (someString) => {
   // console.log('For string %s loop count is %s', someString, loopCount);
   return maxLength;
 }
-console.log(findLongestSubstring('')) // 0; 
-console.log(findLongestSubstring('rithmschool')) // 7; 
+// console.log(findLongestSubstring('')) // 0; 
+// console.log(findLongestSubstring('rithmschool')) // 7; 
 console.log(findLongestSubstring('thisisawesome')) // 6; 
-console.log(findLongestSubstring('thecatinthehat')) // 7; 
-console.log(findLongestSubstring('bbbbbb')) // 1; 
-console.log(findLongestSubstring('longestsubstring')) // 8; 
-console.log(findLongestSubstring('thisishowwedoit')) // 6; 
+// console.log(findLongestSubstring('thecatinthehat')) // 7; 
+// console.log(findLongestSubstring('bbbbbb')) // 1; 
+// console.log(findLongestSubstring('longestsubstring')) // 8; 
+// console.log(findLongestSubstring('thisishowwedoit')) // 6; 
 module.exports = { findLongestSubstring };
