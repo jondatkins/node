ad54dde488217746e98975cf561b751585377a48
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2025-06-09 22:14:44 +0100

    This is a fix for the flatten array problem, courtesy of Neetcode.

--- algorithms/modules/flatten.js
+++ algorithms/modules/flatten.js
@@ -1,9 +1,11 @@
 var flatten = function (arr, n) {
   const res = [];
-
+  // If n is not passed to the function, assume that you want a wholly flattened array.
+  if (typeof n === 'undefined') {
+    n = Number.MAX_VALUE;
+  }
   function helper(arr, depth) {
     for (const val of arr) {
-      // if (typeof val === 'object' && depth < n) {
       if (Array.isArray(val) && depth < n) {
         helper(val, depth + 1)
       }
@@ -15,8 +17,8 @@ var flatten = function (arr, n) {
   }
   return helper(arr, 0);
 }
-console.log(flatten([1, 2, 3, [4, 5]], 0)) // [1, 2, 3, 4, 5]; 
-// console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]; 
-// console.log(flatten([[1], [2], [3]])) // [1,2,3]; 
+// console.log(flatten([1, 2, 3, [4, 5]], 0)) // [1, 2, 3, 4, 5]; 
+// console.log(flatten([1, [2, [3, 4], [[5]]]], 5)) // [1, 2, 3, 4, 5]; 
+// console.log(flatten([[1], [2], [3]], 1)) // [1,2,3]; 
 // console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3; 
 module.exports = { flatten };
--- algorithms/tests/flatten.test.js
+++ algorithms/tests/flatten.test.js
@@ -1,14 +1,14 @@
 const flatten = require('../modules/flatten')
 
 test(`testDescription`, () => {
-  expect(flatten.flatten([1, 2, 3, [4, 5]])).toBe([1, 2, 3, 4, 5])
+  expect(flatten.flatten([1, 2, 3, [4, 5]])).toEqual([1, 2, 3, 4, 5])
 })
 test(`testDescription`, () => {
-  expect(flatten.flatten([1, [2, [3, 4], [[5]]]])).toBe([1, 2, 3, 4, 5])
+  expect(flatten.flatten([1, [2, [3, 4], [[5]]]])).toEqual([1, 2, 3, 4, 5])
 })
 test(`testDescription`, () => {
-  expect(flatten.flatten([[1], [2], [3]])).toBe([1, 2, 3])
+  expect(flatten.flatten([[1], [2], [3]])).toEqual([1, 2, 3])
 })
 test(`testDescription`, () => {
-  expect(flatten.flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])).toBe([1, 2, 3])
+  expect(flatten.flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])).toEqual([1, 2, 3])
 })
