9364d6b8bcc58a6e189bca7428b7b60154eb1763
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2025-06-15 20:44:12 +0100

    Small changes to capitalize words in line with the course solution. Plus the .slice() call is redundant.

--- algorithms/modules/capitalizeWords.ts
+++ algorithms/modules/capitalizeWords.ts
@@ -1,10 +1,11 @@
 let words = ['i', 'am', 'learning', 'recursion'];
 const capitalizeWords = (arr: string[]): string[] => {
   if (arr.length === 1) {
-    arr[0] = arr[0].toUpperCase();
-    return arr;
+    return [arr[0].toUpperCase()];
   }
-  var newArray = capitalizeWords(arr.slice(1)).slice();
+  // remove the first array element, make the recursive call
+  var newArray = capitalizeWords(arr.slice(1));
+  // Add the first word of the array back with the word capitalized.
   newArray.unshift(arr[0].toUpperCase());
   return newArray;
 }
