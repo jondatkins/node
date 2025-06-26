43c306b38e0ab46c7bc852e2d3dc66aa93caf6de
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2025-06-26 15:04:20 +0100

    Shorter, more concise fix based on course solution.

--- algorithms/modules/selectionSort.ts
+++ algorithms/modules/selectionSort.ts
@@ -1,20 +1,13 @@
 const selectionSort = (arr: number[]): number[] => {
   for (let i = 0; i < arr.length; i++) {
     let min = i;
-    let newMin = i;
+    // let newMin = i;
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[newMin]) {
-        newMin = j;
+      if (arr[j] < arr[min]) {
+        min = j;
       }
     }
-    if (arr[min] !== arr[newMin]) {
-      console.log(`***********************************`);
-      console.log(arr);
-      console.log(`Swapping To:`);
-      swap(arr, min, newMin);
-      console.log(arr);
-      console.log(`***********************************`);
-    }
+    swap(arr, i, min);
   }
   return arr;
 }
@@ -23,6 +16,6 @@ function swap(arr: number[], x: number, y: number): void {
   [arr[x], arr[y]] = [arr[y], arr[x]];
 }
 console.log(selectionSort([5, 8, 2, 1, 9])) // [1,2,5,8,9]; 
-// console.log(selectionSort([8, 2, 1, 9])) // [1,2,8,9]; 
-// console.log(selectionSort([1, 2, 1, 9])) // [1,1,2,9]; 
+console.log(selectionSort([8, 2, 1, 9])) // [1,2,8,9]; 
+console.log(selectionSort([1, 2, 1, 9])) // [1,1,2,9]; 
 module.exports = { selectionSort };
