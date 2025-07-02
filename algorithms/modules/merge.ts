b850dd6dad5da2be434ae9e88461bb9038eff023
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2025-07-02 20:19:04 +0100

    This is the fix for merge sort, needs tidying but works.

--- algorithms/modules/merge.ts
+++ algorithms/modules/merge.ts
@@ -7,28 +7,45 @@ const mergeHelper = (arr1: number[], arr2: number[]): number[] => {
       sortedArray.push(arr1[i]);
       i++;
     }
-    // i is greater than or equal to i
+    // i is greater than or equal to j
     else {
       sortedArray.push(arr2[j]);
       j++;
     }
   }
-  if (i < arr1.length - 1) {
-    // slice arr 1 at i, and join it to the end of sortedArray
-    sortedArray = [...sortedArray, ...arr1.slice(i, arr1.length)];
+  // if (i < arr1.length - 1) {
+  //   // slice arr 1 at i, and join it to the end of sortedArray
+  //   sortedArray = [...sortedArray, ...arr1.slice(i, arr1.length)];
+  // }
+  // if (j < arr2.length - 1) {
+  //   // slice arr 1 at i, and join it to the end of sortedArray
+  //   sortedArray = [...sortedArray, ...arr2.slice(j, arr2.length)];
+  // }
+  while (i < arr1.length) {
+    sortedArray.push(arr1[i]);
+    i++;
   }
-  if (j < arr2.length - 1) {
-    // slice arr 1 at i, and join it to the end of sortedArray
-    sortedArray = [...sortedArray, ...arr2.slice(j, arr2.length)];
+  while (j < arr2.length) {
+    sortedArray.push(arr2[j]);
+    j++;
   }
   return sortedArray;
 }
 
-function merge() {
-  // body of arrow function
+function merge(arr: number[]): number[] {
+  if (arr.length === 1) {
+    return arr;
+  }
+  let arr1 = arr.slice(0, arr.length / 2);
+  let arr2 = arr.slice(arr.length / 2, arr.length);
+  let arr3 = merge((arr1));
+  let arr4 = merge((arr2));
+  return mergeHelper(arr3, arr4);
 }
 
-console.log(mergeHelper([1, 10, 50], [2, 14, 99, 100])) // [1,2,14,50,99,100]; 
-console.log(mergeHelper([1, 10, 50, 77], [2, 8, 14, 99, 100])) // [1, 10, 2, 8,14,50,99,100]; 
-console.log(mergeHelper([1, 10, 50, 77], [14, 99, 100])) // [1, 10, 14,50,99,100]; 
+// console.log(mergeHelper([1, 10, 50], [2, 14, 99, 100])) // [1,2,14,50,99,100]; 
+// console.log(mergeHelper([1, 10, 50, 77], [2, 8, 14, 99, 100])) // [1, 10, 2, 8,14,50,99,100]; 
+// console.log(mergeHelper([1, 10, 50, 77], [14, 99, 100])) // [1, 10, 14,50,99,100]; 
+let testArr1 = [5, 2, 9, 1]; // [1,2,5,9]
+console.log(merge(testArr1));
 module.exports = { mergeHelper };
