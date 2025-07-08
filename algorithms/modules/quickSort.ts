531e570a563f8820967cf3ea15f5f946befb7c70
jondatkins <jon.david.atkins@gmail.com>
2025-07-08 18:36:04 +0100

    Adding some better tests. Changing code in line with course solution.

--- algorithms/modules/quickSort.ts
+++ algorithms/modules/quickSort.ts
@@ -26,12 +26,11 @@ const pivotHelper = <Type>(arr: Type[], left = 0, right = arr.length - 1): numbe
 // right.Then make a recursive call for the left and right side of the subarray,
 // excluding the pivot.If the subarray is 0 length, just return the array.
 function quickSort<Type>(arr: Type[], left = 0, right = arr.length - 1): Type[] {
-  if (right - left < 1) {
-    return arr;
+  if (left < right) {
+    let pivotIndex = pivotHelper(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
   }
-  let pivotIndex = pivotHelper(arr, left, right);
-  quickSort(arr, left, pivotIndex - 1);
-  quickSort(arr, pivotIndex + 1, right);
   return arr;
 }
 
@@ -43,15 +42,15 @@ let arr1 = [9, 3, 12, 7, 1, 44, 8]
 let arr2 = [22, -22, 9, 3, 12, 15, 99999, 7, 33, 1, 44, 8]
 let arr3 = [4, 8, 2, 1, 5, 7, 6, 3];
 let arr4 = [5, 2, 1, 8, 4, 7, 6, 3];
-console.log(`unsorted: ${arr1}`);
-console.log(`sorted:   ${quickSort(arr1)}`)
-console.log();
-console.log(`unsorted: ${arr2}`);
-console.log(`sorted:   ${quickSort(arr2)}`)
-console.log();
-console.log(`unsorted: ${arr3}`);
-console.log(`sorted:   ${quickSort(arr3)}`)
-console.log();
-console.log(`unsorted: ${arr4}`);
-console.log(`sorted:   ${quickSort(arr4)}`)
+// console.log(`unsorted: ${arr1}`);
+// console.log(`sorted:   ${quickSort(arr1)}`)
+// console.log();
+// console.log(`unsorted: ${arr2}`);
+// console.log(`sorted:   ${quickSort(arr2)}`)
+// console.log();
+// console.log(`unsorted: ${arr3}`);
+// console.log(`sorted:   ${quickSort(arr3)}`)
+// console.log();
+// console.log(`unsorted: ${arr4}`);
+// console.log(`sorted:   ${quickSort(arr4)}`)
 module.exports = { pivotHelper, quickSort };
--- algorithms/tests/quickSort.test.js
+++ algorithms/tests/quickSort.test.js
@@ -16,6 +16,9 @@ test(`I expect 9 at index 4, with lower values to the left\n${arr1}`, () => {
 test(`I expect 22 at index 8, with lower values to the left\n${arr2}`, () => {
   expect(arr2).toEqual([8, -22, 9, 3, 12, 15, 7, 1, 22, 99999, 44, 33])
 })
-test(``, () => {
+test(`Array 1 ${arr1} should be sorted`, () => {
   expect(quickSort.quickSort(arr1)).toEqual([1, 3, 7, 8, 9, 12, 44])
+})
+test(`Array 2 ${arr2} should be sorted`, () => {
+  expect(quickSort.quickSort(arr2)).toEqual([-22, 1, 3, 7, 8, 9, 12, 15, 22, 33, 44, 99999])
 })
