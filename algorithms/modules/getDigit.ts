f31b5c0f105d846f47939982d8a76dc09b63ad3b
jondatkins <jon.david.atkins@gmail.com>
2025-07-09 20:31:02 +0100

    Helper functions for radix sort

--- algorithms/modules/getDigit.ts
+++ algorithms/modules/getDigit.ts
@@ -1,14 +1,29 @@
 const getDigit = (num: number, digit: number) => {
   if (digit > 0) {
+    num = Math.abs(num);
     num = Math.floor(num / (10 ** digit)); // gets you the second digit
   }
   return num % 10;
 }
 
-console.log(getDigit(12345, 0)) // 5; 
-console.log(getDigit(12345, 1)) // 4; 
-console.log(getDigit(12345, 2)) // 3; 
-console.log(getDigit(12345, 3)) // 2; 
-console.log(getDigit(12345, 4)) // 1; 
-console.log(getDigit(12345, 5)) // 0; 
-module.exports = { getDigit };
+const digitCount = (num: number) => {
+  let digitNum = 0;
+  while (num > 0) {
+    num = Math.floor(num / 10);
+    digitNum++;
+  }
+  return digitNum;
+}
+
+console.log(digitCount(123)); // 3
+console.log(digitCount(12)); // 2
+console.log(digitCount(1)); // 1
+
+// console.log(getDigit(12345, 0)) // 5; 
+// console.log(getDigit(12345, 1)) // 4; 
+// console.log(getDigit(12345, 2)) // 3; 
+// console.log(getDigit(12345, 3)) // 2; 
+// console.log(getDigit(12345, 4)) // 1; 
+// console.log(getDigit(12345, 5)) // 0; 
+// console.log(getDigit(-12345, 5)) // 0; 
+module.exports = { getDigit, digitCount };
--- algorithms/tests/getDigit.test.js
+++ algorithms/tests/getDigit.test.js
@@ -1,20 +1,32 @@
 const getDigit = require('../modules/getDigit')
 
 test(`testDescription`, () => {
-expect( getDigit.getDigit(12345, 0)).toEqual(5)
+  expect(getDigit.getDigit(12345, 0)).toEqual(5)
 })
 test(`testDescription`, () => {
-expect( getDigit.getDigit(12345, 1)).toEqual(4)
+  expect(getDigit.getDigit(12345, 1)).toEqual(4)
 })
 test(`testDescription`, () => {
-expect( getDigit.getDigit(12345, 2)).toEqual(3)
+  expect(getDigit.getDigit(12345, 2)).toEqual(3)
 })
 test(`testDescription`, () => {
-expect( getDigit.getDigit(12345, 3)).toEqual(2)
+  expect(getDigit.getDigit(12345, 3)).toEqual(2)
 })
 test(`testDescription`, () => {
-expect( getDigit.getDigit(12345, 4)).toEqual(1)
+  expect(getDigit.getDigit(12345, 4)).toEqual(1)
 })
 test(`testDescription`, () => {
-expect( getDigit.getDigit(12345, 5)).toEqual(0)
+  expect(getDigit.getDigit(12345, 5)).toEqual(0)
+})
+test(`testDescription`, () => {
+  expect(getDigit.getDigit(-12345, 5)).toEqual(0)
+})
+test(`testDescription`, () => {
+  expect(getDigit.digitCount(123)).toEqual(3)
+})
+test(`testDescription`, () => {
+  expect(getDigit.digitCount(12)).toEqual(2)
+})
+test(`testDescription`, () => {
+  expect(getDigit.digitCount(1)).toEqual(1)
 })
