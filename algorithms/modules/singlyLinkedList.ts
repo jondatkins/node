6c1af706d4b66d9e1ca73f0e1c016a3d7b1cce22
jondatkins <jon.david.atkins@gmail.com>
2025-07-14 20:53:52 +0100

    Starting on linked lists.

---
+++ algorithms/modules/singlyLinkedList.ts
@@ -0,0 +1,98 @@
+const repl = require('node:repl');
+
+// repl.start();
+class Node<T> {
+
+  public val: T;
+  public next: Node<T> | null = null;
+  constructor(val: T) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// class Node<T> {
+//   public next: Node<T> | null = null;
+//   public prev: Node<T> | null = null;
+//   constructor(public data: T) { }
+// }
+
+var first = new Node("Hi");
+first.next = new Node("there");
+first.next.next = new Node("there");
+
+class SinglyLinkedList<T> {
+  public head: Node<T> | null;
+  public tail: Node<T> | null;
+  public length: number;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val: T) {
+    let node = new Node<T>(val);
+
+    if (!this.head || !this.tail) {
+      this.head = node;
+      this.tail = this.head;
+    }
+    else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop(): Node<T> | null {
+    if (!this.head) {
+      return null;
+    }
+    let prev = this.head;
+    let next = this.head.next;
+    if (!next) {
+      this.head = null;
+      this.tail = null;
+      // Just set to 0?
+      this.length--;
+      return prev;
+    }
+    while (next.next) {
+      prev = next;
+      next = next.next;
+    }
+    let returnNode = this.tail;
+    this.tail = prev;
+    this.tail.next = null;
+    this.length--;
+    // console.log(prev);
+    return returnNode;
+  }
+
+}
+
+let list = new SinglyLinkedList();
+list.push("Foo");
+// console.log(`Pushing 'Foo'`);
+// console.log(list);
+list.push("Bar");
+// console.log(`Pushing 'Bar'`);
+// console.log(list);
+list.push("Pushkin");
+// console.log(`Pushing 'Pushkin'`);
+// console.log(list);
+
+let pushkin = list.pop();
+console.log(`Popping 'Pushkin'`);
+console.log(pushkin);
+console.dir(list);
+let bar = list.pop();
+console.log(`Popping 'Bar'`);
+console.log(bar);
+console.dir(list);
+let foo = list.pop();
+console.log(`Popping 'Foo'`);
+console.log(foo);
+console.dir(list);
