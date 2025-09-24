dc7fd49a7f579d1d923dd124621487993d276787
jondatkins <122027005+jondatkins@users.noreply.github.com>
2025-09-24 21:56:35 +0100

    Working on removing nodes, depth first search

--- algorithms/modules/Graph.ts
+++ algorithms/modules/Graph.ts
@@ -1,13 +1,3 @@
-interface EdgeMap {
-  nodeName: string;
-  edgeList: string[];
-}
-
-interface AdjacencyList {
-  key: string;
-  value: EdgeMap;
-}
-
 class Graph<T extends string | number> {
   adjacencyList: Record<T, T[]> = {} as Record<T, T[]>;
 
@@ -42,23 +32,137 @@ class Graph<T extends string | number> {
     let edges2 = this.adjacencyList[vertex2].filter(elem => elem !== vertex1);
     this.adjacencyList[vertex2] = edges2;
   }
+
+  // Loop over adjacencyList for the 
+  // vertex. For each edge, remove the edge 
+  // using remove edge with the vertex's name
+  removeVertex(vertex: T) {
+    while (this.adjacencyList[vertex].length) {
+      let linkedVertex = this.adjacencyList[vertex].pop();
+      if (linkedVertex) {
+        this.removeEdge(vertex, linkedVertex);
+      }
+    }
+    delete this.adjacencyList[vertex];
+  }
+
+  depthFirstSearch(vertex: T) {
+    let results: T[] = [];
+    const searchHelper = (vertex: T) => {
+      if (!vertex) {
+        return;
+      }
+      results.push(vertex);
+      let linkedVertices = this.adjacencyList[vertex];
+      for (const node of linkedVertices) {
+        if (results.indexOf(node) === -1) {
+          searchHelper(node);
+        }
+
+      }
+    }
+    searchHelper(vertex);
+    return results;
+  }
+
+  depthFirstSearchCourse(start: T) {
+    let results: T[] = [];
+    const visited: Record<string, boolean> = {}; // or Record<T, boolean> if T is a string/number
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex: T) {
+      if (!vertex) {
+        return null;
+      }
+      visited[vertex as unknown as string] = true;
+      results.push(vertex);
+      adjacencyList[vertex].forEach(neighbour => {
+        if (!visited[neighbour as string]) {
+          return dfs(neighbour);
+        }
+      })
+    })(start)
+    return results;
+  }
+
+  depthFirstSearchCourse2(start: T): T[] {
+    const results: T[] = [];
+    const visited: Record<string, boolean> = {}; // or Map<T, boolean>
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex: T) {
+      if (!vertex) return;
+      visited[vertex as unknown as string] = true;
+      results.push(vertex);
+
+      for (const neighbour of adjacencyList[vertex]) {
+        if (!visited[neighbour as unknown as string]) {
+          dfs(neighbour);
+        }
+      }
+    })(start)
+
+    // dfs(start);
+    return results;
+  }
+
+
 }
 
+// testGraph.addVertex("Tokyo");
+// testGraph.addVertex("Dallas");
+// testGraph.addVertex("Aspen");
+// testGraph.addEdge("Dallas", "Tokyo");
+// testGraph.addEdge("Dallas", "Aspen");
+// testGraph.removeEdge("Aspen", "Dallas");
+// testGraph.removeEdge("Dallas", "Tokyo");
+// testGraph.removeVertex("Dallas");
+// testGraph.depthFirstSearch("Tokyo");
+
+// B - A - E
+// | \   / |
+// C - D - F
+//
+// Starting from A, go to B. Then C, D, E, F.
 let testGraph = new Graph();
-testGraph.addVertex("Tokyo");
-testGraph.addVertex("Dallas");
-testGraph.addVertex("Aspen");
+testGraph.addVertex("B")
+testGraph.addVertex("A")
+testGraph.addVertex("E")
+testGraph.addVertex("C")
+testGraph.addVertex("D")
+testGraph.addVertex("F")
+testGraph.addEdge("B", "A")
+testGraph.addEdge("A", "E")
+testGraph.addEdge("B", "C")
+testGraph.addEdge("B", "D")
+testGraph.addEdge("C", "D")
+testGraph.addEdge("D", "E")
+testGraph.addEdge("D", "F")
+testGraph.addEdge("E", "F")
+let depthFirst1 = testGraph.depthFirstSearch("A")
+// console.log(depthFirst1);
 
-testGraph.addEdge("Dallas", "Tokyo");
-testGraph.addEdge("Dallas", "Aspen");
-testGraph.removeEdge("Aspen", "Dallas");
-testGraph.removeEdge("Dallas", "Tokyo");
-// testGraph.addVertex("A");
-// testGraph.addVertex("B");
-// testGraph.addVertex("C");
-// testGraph.addVertex("D");
-// testGraph.addVertex("E");
-// testGraph.addVertex("F");
-console.log(testGraph.adjacencyList);
-
-module.exports = {};
+// B - A - C
+// |       |
+// D ----- E
+//  \     /
+//     F
+//
+// A, B, D, E, C, (Dead End, go back to E), F
+let testGraph2 = new Graph();
+testGraph2.addVertex("A")
+testGraph2.addVertex("B")
+testGraph2.addVertex("C")
+testGraph2.addVertex("D")
+testGraph2.addVertex("E")
+testGraph2.addVertex("F")
+testGraph2.addEdge("A", "B")
+testGraph2.addEdge("A", "C")
+testGraph2.addEdge("B", "D")
+testGraph2.addEdge("C", "E")
+testGraph2.addEdge("D", "E")
+testGraph2.addEdge("D", "F")
+testGraph2.addEdge("F", "E")
+let depthFirst = testGraph2.depthFirstSearchCourse("A")
+console.log(depthFirst);
+module.exports = { Graph };
