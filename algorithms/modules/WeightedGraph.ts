2ec5755adbb0d5d03a83fb35df1072a4ed79a97c
jondatkins <122027005+jondatkins@users.noreply.github.com>
2025-09-29 19:55:13 +0100

    Starting on Dijkstra's algorithm

---
+++ algorithms/modules/WeightedGraph.ts
@@ -0,0 +1,39 @@
+const util = require("util");
+
+interface WeightedEdge<T> {
+  node: T;
+  weight: number;
+}
+
+type WeightedAdjacencyList<T extends string | number> = {
+  [vertex in T]: WeightedEdge<T>[];
+};
+
+class WeightedGraph<T extends string | number> {
+  adjacencyList: WeightedAdjacencyList<T> = {} as WeightedAdjacencyList<T>;
+
+  addVertex(vertex: T) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(vertex1: T, vertex2: T, weight: number) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    // for undirected graphs:
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+}
+
+
+let graph = new WeightedGraph();
+graph.addVertex("A")
+graph.addVertex("B")
+graph.addVertex("C")
+graph.addEdge("A", "B", 9)
+graph.addEdge("A", "C", 5)
+graph.addEdge("B", "C", 7)
+console.dir(graph, { depth: null, colors: true });
+// console.log(util.inspect(graph, { depth: null, colors: true }));
+// console.log(JSON.stringify(graph, null, 2))
+module.exports = { WeightedGraph };
