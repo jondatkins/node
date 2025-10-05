f99ccc5c3528e88787d336292ee0455480d4ae75
jondatkins <122027005+jondatkins@users.noreply.github.com>
2025-10-05 21:08:10 +0100

    Memoization of fib sequence

--- algorithms/modules/fibonacci.ts
+++ algorithms/modules/fibonacci.ts
@@ -1,14 +1,47 @@
 // Return the nth number in the fibonacci sequence
-const fibonacci = (num: number): number => {
-  if (num <= 2) {
-    return 1;
+const fibonacci = (num: number, isUseMemo = true): number => {
+  let numCalls = 0;
+  let fibNums: Record<number, number> = {} as Record<number, number>;
+
+  const fibonacciHelper = (num: number): number => {
+    if (num <= 2) {
+      return 1;
+    }
+    numCalls++;
+
+    if (!isUseMemo) {
+      return fibonacciHelper(num - 1) + fibonacciHelper(num - 2);
+    }
+    else {
+      if (fibNums[num]) {
+        return fibNums[num]
+      }
+      numCalls++;
+      let result = fibonacciHelper(num - 1) + fibonacciHelper(num - 2);
+      fibNums[num] = result;
+      return result;
+    }
   }
-  return fibonacci(num - 1) + fibonacci(num - 2);
+  let fibNum = fibonacciHelper(num);
+  // console.log(numCalls);
+  return fibNum;
 }
 
-console.log(fibonacci(4)) // 3; 
-console.log(fibonacci(10)) // 55; 
-console.log(fibonacci(28)) // 317811; 
-console.log(fibonacci(35)) // 9227465; 
+let numCalls = 0;
+function fib(n: number, memo: number[] = []): number {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  numCalls++;
+  let res = fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
+
+// console.log(fibonacci(4)) // 3; 
+// console.log(fibonacci(10)) // 55; 
+// console.log(fibonacci(28)) // 317811; 
+// console.log(fibonacci(35, false)) // 9227465; 
+console.log(fibonacci(100)) // 9227465; 
+console.log(numCalls);
 
 module.exports = { fibonacci };
